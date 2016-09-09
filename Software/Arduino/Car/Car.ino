8073e742afed8ffad78e48e3d67a4b04ebfc78e4
Ezequiel Blanco <kiel814@gmail.com>
2016-09-09 12:41:01 -0300

    minor changes in Arduino logic
    also generated new apk

--- Software/Arduino/Car/Car.ino
+++ Software/Arduino/Car/Car.ino
@@ -5,29 +5,35 @@ const unsigned int BWD = 8;
 const unsigned int SRV = 6;
 const unsigned int EN = 9;
 
-const unsigned int BACKWARDS = 0;
-const unsigned int FORWARD = 1;
-    
+const int MIN_THROTTLE = 10;
+
 Servo myservo;
+
+bool bFirstRead; // This indicates that data was received at least once.
+int iLastTransmission; // Checkpoint to indicate when we last received data.
 
 void setup()
 {
   pinMode(FWD, OUTPUT);
   pinMode(BWD, OUTPUT);
   
-  Serial.begin(9600);  // initialize serial: 
+  Serial.begin(9600);  // Initialize serial: 
   myservo.attach(SRV);
 
   digitalWrite( FWD, LOW );
   digitalWrite( BWD, LOW );
   analogWrite( EN, LOW );
   myservo.write(90);
+  
+  bFirstRead = false;
+  iLastTransmission = 0;
 }
 
 void loop()
 {  
   if( Serial.available() > 0 )
   {
+    bFirstRead = true;
     char cReading = Serial.read();
     
     switch(cReading)
@@ -35,42 +41,55 @@ void loop()
       case 'A':
       {
         int iAngle = Serial.parseInt();
-        iAngle = constrain(iAngle, 32, 160);//for this stearing we cant go further than these values
+        iAngle = constrain(iAngle, 32, 160); // Steering servo supported range.
         myservo.write(iAngle);
         break;
       }
       case 'G':
       {
         bool bGear = ( bool )Serial.parseInt();
-        digitalWrite( FWD, LOW );
-        digitalWrite( BWD, LOW );
-        digitalWrite( FWD, bGear );
-        digitalWrite( BWD, !bGear );
+        if( bGear )
+        {
+          digitalWrite( BWD, LOW );
+          digitalWrite( FWD, HIGH );
+        }
+        else
+        {
+          digitalWrite( FWD, LOW );
+          digitalWrite( BWD, HIGH );
+        }
         break;
       }
       case 'T':
       {
         int iThrottle = Serial.parseInt();
-        iThrottle = constrain( iThrottle, 10, 255 );
+        if( iThrottle > 0 )
+        {
+          iThrottle = iThrottle * ( 255 - MIN_THROTTLE ) / 254 + MIN_THROTTLE;
+        }
         analogWrite( EN, iThrottle );
         break;
       }
       default:
       {
-        //ignore the reading...
+        // Not a valid command. Ignore.
         break;
       }
-    }    
+    }
+    
+    // Data received. Reset checkpoint.
+    iLastTransmission = millis();
   }
-
-  // if been two seconds without receiving data, either valid or not, set everything to low
-  /*
-  if( passedTime > 2000 )
+  
+  int passedTime = millis() - iLastTransmission;
+  if( bFirstRead && passedTime > 2000 )
   {
+    // More than 2 seconds have passed since the last transmission.
+    // Assume connection was lost.
     digitalWrite( FWD, LOW );
     digitalWrite( BWD, LOW );
     analogWrite( EN, LOW );
     myservo.write(90);
+    bFirstRead = false;
   }
-  */
 }
